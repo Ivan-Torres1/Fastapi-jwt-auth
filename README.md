6d589abdaa36aec7172da562f85b9eb2dabdf01c
Ivan Torres <ivantorres2076@gmail.com>
2023-12-10 11:37:48 -0300

    Cambiando pymysql al ORM 'SQLmodel' para mayor comodidad y eficiencia en la API. Aun falta terminar pero guardo los datos

--- README.md
+++
@@ -1,3 +0,0 @@
-# ivanapi
-Hola, este es mi repositorio donde hago todas mis practicas y muestro todo mi aprendisaje en API. En mi caso, estoy usando el framework de python llamado 
-FASTAPI, el cual, es muy facil y practico para usar. Gracias por leer :)
--- proyect_python_fastAPI.py
+++ proyect_python_fastAPI.py
@@ -1,18 +1,17 @@
 
 from fastapi import FastAPI,Depends, HTTPException,status
-from pydantic import BaseModel,EmailStr,Field
-from re import Pattern
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt,JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from typing import Optional
-import pymysql
+from pymysql import Error
 from envparse import Env
 from typing import Type
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, create_engine,Session,Field,select 
+from pydantic import EmailStr
 
-
+# TERMINA DE HACER LOS EXECUTE A LA BASE DE DATOS, ARREGLA LA FUNCION VERIFYUSER Y MAS
 
 
 """
@@ -51,31 +50,79 @@ crypt = CryptContext(schemes=["bcrypt"])
 
 
 
+url_db = f"mysql+pymysql://{useer}:{password}@{host}/{database}"
+engine = create_engine(url=url_db,echo=True)
 
-def get_db():
-    db = pymysql.connect(host=host, port=3306, user=useer, db=database, passwd=password)
+
+
+
+
+ #                                                                          SQL MODEL - MODELO DE TABLA
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(
+        default=None, 
+        primary_key=True)
+    
+    name: str = Field(
+        min_length=2,
+        max_length=40,
+        regex="^[A-Za-z]+$")
+    
+    lastname: str = Field(
+        min_length=2, 
+        max_length=40, 
+        regex="^[A-Za-z]+$")
+    
+    password: str = Field(
+        min_length=6, 
+        regex="^(?=.*[A-Z])(?=.*[0-9]).*$")
+    email: str = EmailStr
+    
+
+# ------------------------------------------------
+class UserValidationEdit(SQLModel, table=True):
+    id: Optional[int] = Field(
+        default=None, 
+        primary_key=True)
+
+    name: Optional[str] = Field(
+        min_length=2,
+        max_length=40, 
+        regex="^[A-Za-z]+$")
+
+    lastname: Optional[str] = Field(
+        min_length=2, 
+        max_length=40, 
+        regex="^[A-Za-z]+$")
+    
+    password: Optional[str] = Field(
+        min_length=6, 
+        regex="^(?=.*[A-Z])(?=.*[0-9]).*$")
+    
+    email: Optional[str] = EmailStr
+
+
+# /////////////////////////////////////////////////////
+
+#                                                                           FUNCIONES
+
+def create_db():
+     SQLModel.metadata.create_all(engine)
+
+
+def Haspassword(password):
+    hasheo = crypt.hash(password)
+    return hasheo
+
+def errordb(e):
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+
+def VerifyUser(emailUser,nameUser):
     try:
-        yield db
-    finally:
-        db.close()
-def get_db_dependency(db: pymysql.connections.Connection = Depends(get_db)):
-    return db
-
-
-
-
-
-
-
-
-#                                       Funciones 
-def VerifyUser(email,name,db):
-    try:
-        with db.cursor() as cursor:
-            cursor.execute("SELECT id FROM users WHERE email=%s or name=%s",[email,name])
-            date = cursor.fetchone()
-
-    except Exception as e:
+        with Session(engine) as session:
+            date = session.exec(select(User).where(User.email == emailUser, User.name == nameUser))
+    except Error as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error al verificar usuario: {str(e)}")
@@ -85,90 +132,34 @@ def VerifyUser(email,name,db):
                         headers={"ERROR":"NOT ACCEPTABLE"})
     
 
-def Haspassword(password):
-    hasheo = crypt.hash(password)
-    return hasheo
 
 
 
-#                           COMPRUEBA SI LA PERSONA SE LOGEO CORRECTAMENTE / COMPRUEBA EL TOKEN
-async def comprobarToken(token: str = Depends(oauth2),db: pymysql.connections.Connection = Depends(get_db)):
+
+
+#                                   COMPRUEBA SI LA PERSONA SE LOGEO CORRECTAMENTE / COMPRUEBA EL TOKEN
+async def comprobarToken(token: str = Depends(oauth2)):
     error = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Credenciales de autenticación inválidas",
         headers={"WWW-Authenticate": "Bearer"})
     
     try:
-        username = jwt.decode(token,SECRET,algorithms=[ALGORITHM]).get("sub")
-        if username is None:
+        usernameDecode = jwt.decode(token,SECRET,algorithms=[ALGORITHM]).get("sub")
+        if usernameDecode is None:
             raise error
     except JWTError:
         raise error
     
     try:
-        with db.cursor() as cursor:
-            cursor.execute("SELECT name,lastname,email,id FROM users WHERE email=%s",[username])
-            usernamee = cursor.fetchone()
-    except pymysql.Error as e:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+        with Session(engine) as session:
+            username = session.exec(select(User.name,User.lastname,User.email,User.id).where(User.email == usernameDecode))
+    except Error as e:
+        errordb(e)
 
-    return usernamee
+    return username
 # ------------------------------------------------------------------------------------------------------
 
-#                           basemodel
 
-class User(BaseModel):
-    name: str = Field(
-        min_length=2,
-        max_length=40,
-        pattern="^[A-Za-z]+$")
-    
-    lastname: str = Field(
-        min_length=2,
-        max_length=40,
-        pattern="^[A-Za-z]+$")
-
-    password: str = Field( 
-        min_length=7,
-        pattern="^(?=.*[A-Z])(?=.*[0-9]).*$")
-        
-    
-    def __get_pydantic_core_schema__(self, cls: Type["BaseModel"], **kwargs):
-        return {
-            "type": "string",
-            "minLength": 7,
-            "pattern": "^(?=.*[A-Z])(?=.*[0-9]).*$"}
-
-
-    email: EmailStr
-
-
-# ------------------------------------------------
-class UserValidationEdit(BaseModel):
-    name: Optional[Field(
-        min_length=2,
-        max_length=40,
-        pattern="^[A-Za-z]+$")]
-    
-    lastname: Optional[Field(min_length=2,
-        max_length=40,
-        pattern="^[A-Za-z]+$")]
-
-    password: Optional[Field( 
-        min_length=7,
-        pattern="^[A-Za-z]+$")]
-    def __get_pydantic_core_schema__(self, cls: Type["BaseModel"], **kwargs):
-        return {
-            "type": "string",
-            "minLength": 7,
-            "pattern": "^(?=.*[A-Z])(?=.*[0-9]).*$"}
-            
-    email: Optional[EmailStr]
-
-
-
-
-# /////////////////////////////////////////////////////
 
 
 @app.get("/")
@@ -176,56 +167,50 @@ async def saludo():
     return {"Hola":"Mundo"}
 
 
-#                                VER TODOS LOS USUARIOS DE LA BASE DE DATOS
+#                                       VER TODOS LOS USUARIOS DE LA BASE DE DATOS
 @app.get("/users")
-async def users(db: pymysql.connections.Connection = Depends(get_db)):
+async def users():
     try:
-        with db.cursor() as cursor:
-            cursor.execute("SELECT * FROM users")
-            users = cursor.fetchall()
-    except pymysql.Error as e:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
-    # print(users, "ANANANANANANANANAN")
+        with Session(engine) as session:
+            users = session.exec(select(User)).all()
+    except Error as e:
+        errordb(e)
     return users                                
 
 
 
 
-#                            RETORNA INFORMACION DE USUARIO LOGEADO
+#                                        RETORNA INFORMACION DE USUARIO LOGEADO
 @app.get("/users/me")
 async def me(user: User = Depends(comprobarToken)):
-    print(user)
     return user
 
+# -------------------------------------------------------------------------------------------------------------------
 
-#                                     REGISTER
-@app.post("/users/register")
-async def registro(user: User,db: pymysql.connections.Connection = Depends(get_db) ):
-    VerifyUser(user.email,user.name,db)
+#                                                REGISTER
+@app.post("/users/registers")
+async def registro(user: User):
+    VerifyUser(user.email,user.name)
     contraseña = Haspassword(user.password)
     try:
-        with db.cursor() as cursor:
-            cursor.execute(
-                "INSERT INTO users (name, lastname, email, password) VALUES (%s, %s, %s, %s)",
-                (user.name, user.lastname, user.email, contraseña))
-        db.commit()
-    except pymysql.Error as e:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+        with Session(engine) as session:
+            usuario = User(name=user.name,lastname=user.lastname,password=user.password,email=user.email)
+            session.add(usuario)
+            session.commit()
+           
+    except Exception as e:
+                errordb(e)
     
     return {"message": "Se registro correctamente"}
 # -------------------------------------------------------------------------------------------------------------------
-#                                    LOGIN
+#                                               LOGIN
 @app.post("/users/login")
-async def login(form: OAuth2PasswordRequestForm = Depends(),db: pymysql.connections.Connection = Depends(get_db)):
+async def login(form: OAuth2PasswordRequestForm = Depends()):
     try:
-        with db.cursor() as cursor:
-            cursor.execute("SELECT email,password FROM users WHERE email=%s",[form.username])
-            user = cursor.fetchone()
-    except pymysql.Error as e:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+        with Session(engine) as session:
+            user = session.exec(select(User.email,User.password).where(User.email == form.username)).all()
+    except Error as e:
+        errordb(e)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     
@@ -240,11 +225,11 @@ async def login(form: OAuth2PasswordRequestForm = Depends(),db: pymysql.connecti
 # -------------------------------------------------------------------------------------------------------------------
 
 
-#                           EDITA LA INFORMACION DE UN USUARIO
+#                                               EDITA LA INFORMACION DE UN USUARIO
 # EJEMPLO 
 # http://127.0.0.1:8000/users/editUser/name?newdate=Aylen
 @app.put("/users/editUser/{columnaName}")
-async def editUsers(columnaName: str, newdate: str, user: User = Depends(comprobarToken),db: pymysql.connections.Connection = Depends(get_db)):
+async def editUsers(columnaName: str, newdate: str, user: User = Depends(comprobarToken)):
     diccio = {columnaName: newdate}
     validation = UserValidationEdit(columnaName=newdate)
     try:
@@ -256,32 +241,27 @@ async def editUsers(columnaName: str, newdate: str, user: User = Depends(comprob
         newdate = Haspassword(newdate)
     elif columnaName == "email":
         try:
-            with db.cursor() as cursor:
-                cursor.execute(f"SELECT id FROM users WHERE {columnaName} = %s",[newdate])
-                date = cursor.fetchone()
-        except pymysql.Error as e:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+            with Session(engine) as session:
+                date = session.exec(select(User.id).where(columnaName == newdate))
+        except Error as e:
+                errordb(e)
         if date:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="El correo ya esta en uso. Por favor, elija otro.")
-            
-        else: 
-            pass
  
     try:
-        with db.cursor() as cursor:
-            cursor.execute(f"UPDATE users SET {columnaName} = %s WHERE id = %s", [newdate, user[3]])
-            db.commit()
-    except pymysql.Error as e:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+        with Session(engine) as session:
+            pass
+            # cursor.execute(f"UPDATE users SET {columnaName} = %s WHERE id = %s", [newdate, user[3]])
+            # db.commit()
+    except Error as e:
+                errordb(e)
     
     raise HTTPException(status_code=status.HTTP_200_OK,detail="Se actualizo correctamente el usuario")
     
     
     
 
-#                           ELIMINAR UN USUARIO
+#                                                   ELIMINAR UN USUARIO
 
 @app.delete("/users/deleteuser/{emailUser}")
 async def deleteUser(emailUser: str,user: User = Depends(comprobarToken),db: pymysql.connections.Connection = Depends(get_db)):
@@ -289,18 +269,16 @@ async def deleteUser(emailUser: str,user: User = Depends(comprobarToken),db: pym
         with db.cursor() as cursor:
             cursor.execute("SELECT id FROM users WHERE email = %s",[emailUser])
             dato = cursor.fetchone()
-    except pymysql.Error as e:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+    except Error as e:
+                errordb(e)
     if dato:
         if emailUser == user[2]:
             try:
                 with db.cursor() as cursor:
                     cursor.execute("DELETE FROM users WHERE email = %s",[emailUser])
                     db.commit()
-            except pymysql.Error as e:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    headers="OCURRIO UN ERROR CON LA BASE DE DATOS, INTENTE MAS TARDE",detail=e)
+            except Error as e:
+                errordb(e)
             raise HTTPException(status_code=status.HTTP_202_ACCEPTED,detail="Usuario eliminado correctamente")
             
         else:
@@ -311,4 +289,5 @@ async def deleteUser(emailUser: str,user: User = Depends(comprobarToken),db: pym
 
 
 
-
+if __name__ == "__main__":
+     create_db()
